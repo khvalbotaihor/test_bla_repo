5f0a06a0a209f09a03a05f73f224970ac97aaf27
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-08-31 07:57:27 +0300

    Updates

---
+++ precache-manifest.534ff17bb4f2951234c1b5b2ab60a6d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da8113c1e1e3844875a4a14c19c5f379",
+    "url": "/test_bla_repo/index.html"
+  },
+  {
+    "revision": "33dd3b1d29f4ed166811",
+    "url": "/test_bla_repo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/test_bla_repo/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_bla_repo/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33dd3b1d29f4ed166811",
+    "url": "/test_bla_repo/static/js/main.cdf96561.chunk.js"
+  },
+  {
+    "revision": "126276ef8df174808e73",
+    "url": "/test_bla_repo/static/js/runtime-main.750be387.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_bla_repo/static/media/logo.5d5d9eef.svg"
+  }
+]);
